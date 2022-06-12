33fc6170aa6c700a87d61da2687fa71a79b9aec3
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-12 21:29:53 +0900

    basic_switch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import "./styles.css";
 
-alert("d");
+let a = +prompt("a?", "");
+
+switch (a) {
+  case 0:
+    alert("0");
+    break;
+  case 1:
+    alert("1");
+    break;
+  case 2:
+  case 3:
+    alert("2,3");
+    break;
+}
