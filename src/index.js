7d07863e631b0811d2efea4a02da88e2873839f3
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-16 00:34:25 +0900

    core_object2 객체 비어있는지 확인

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import "./styles.css";
 
-let user = new Object();
+let schedule = {};
 
-user.name = "John";
-user.surname = "Smith";
+alert(isEmpty(schedule)); // true
 
-user.name = "Pete";
+schedule["8:30"] = "get up";
 
-delete user.name;
+alert(isEmpty(schedule)); // false
 
-for (let key in user) {
-  alert(key);
-  alert(user[key]);
+function isEmpty(obj) {
+  for (let key in obj) {
+    //if (key !== undefined) return false;
+    //반복문 실행하다 프로퍼티 하나라도 있으면 그 즉시 false 반환
+    return false;
+  }
+  return true;
 }
