64e17c86bca29114aa854f2fde41cd74cf8ac83e
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-16 18:33:00 +0900

    core_object5 체이닝

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import "./styles.css";
 
-let calculator = {
-  read() {
-    this.add1 = prompt("더할 값1", "0");
-    this.add2 = prompt("더할 값2", "0");
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+    return this;
   },
-  sum() {
-    return +this.add1 + +this.add2;
+  down() {
+    this.step--;
+    return this;
   },
-  mul() {
-    return this.add1 * this.add2;
+  showStep: function () {
+    // 사다리에서 몇 번째 단에 올라와 있는지 보여줌
+    alert(this.step);
+    return this;
   }
 };
 
-calculator.read();
-
-alert(calculator.sum());
-alert(calculator.mul());
+ladder.up().up().showStep();
