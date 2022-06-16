ae0593f8807faf63d2b25e4c88389db63f34498b
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-16 21:42:41 +0900

    core_object7 누산기 만들기

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import "./styles.css";
 
-let calculator = new Calculator();
-calculator.read();
+function Accumulator(startingValue) {
+  this.value = startingValue;
 
-alert("Sum=" + calculator.sum());
-alert("Mul=" + calculator.mul());
-
-function Calculator() {
   this.read = function () {
-    this.data1 = prompt("값1", "0");
-    this.data2 = prompt("값2", "0");
-  };
-  this.sum = function () {
-    return +this.data1 + +this.data2;
-  };
-  this.mul = function () {
-    return this.data1 * this.data2;
+    this.data = prompt("값 입력", "0");
+    this.value += +this.data;
   };
 }
+
+let accumulator = new Accumulator(1); // 최초값: 1
+
+accumulator.read(); // 사용자가 입력한 값을 더해줌
+accumulator.read(); // 사용자가 입력한 값을 더해줌
+
+alert(accumulator.value); // 최초값과 사용자가 입력한 모든 값을 더해 출력함
