8c7ad4e021580f6db128b3d465b0f041018a7e63
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-16 18:22:20 +0900

    core_object4 수정

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import "./styles.css";
 
 let calculator = {
   read() {
-    this.add1 = prompt("더할 값1", "");
-    this.add2 = prompt("더할 값2", "");
+    this.add1 = prompt("더할 값1", "0");
+    this.add2 = prompt("더할 값2", "0");
   },
   sum() {
     return +this.add1 + +this.add2;
