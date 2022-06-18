ee55ebe45c13e6d41a529ab64585b53b36437888
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-18 18:35:10 +0900

    core_datatype_string 스팸 문자열 걸러내기

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import "./styles.css";
 
-function ucFirst(str) {
-  if (!str) return str; //빈 문자열일 경우
+function checkSpam(str) {
+  if (!str) return str;
+
+  let lowerStr = str.toLowerCase();
 
   /*
-  let newStr = str[0].toUpperCase();
-
-  for (let i = 1; i < str.length; i++) {
-    newStr = newStr + str[i];
-  }
-  return newStr;
+  if (lowerStr.includes("viagra") || lowerStr.includes("xxx")) return true;
+  else return false;
   */
-
-  return str[0].toUpperCase() + str.slice(1);
+  return lowerStr.includes("viagra") || lowerStr.includes("xxx");
 }
 
-let input = prompt("영문 소문자입력", "john");
-alert(ucFirst(input));
+let input = prompt("입력", "");
+
+alert(checkSpam(input));
+
+//입력 다 소문자로 바꾸고 비교해보기
