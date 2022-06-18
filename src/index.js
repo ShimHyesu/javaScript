8af3aecb04740e594912b55d1e2c67bb3a7d27a2
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-18 21:21:50 +0900

    core_datatype_string 숫자만 추출하기

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import "./styles.css";
 
-function truncate(str, maxlength) {
+function extractCurrencyValue(str) {
   if (!str) return str;
 
-  /*
-  let newStr = str.slice(0, maxlength - 1);
-
-  if (str.length > maxlength) {
-    newStr += "...";
+  let index;
+  for (index = 0; index < str.length; index++) {
+    if (isFinite(str[index])) break;
   }
-
-  return newStr;
-  */
-
-  return str.length > maxlength ? str.slice(0, maxlength - 1) + "..." : str;
+  return str.slice(index);
 }
 
 let input = prompt("입력", "");
-let length = prompt("길이", "");
 
-alert(truncate(input, length));
+alert(extractCurrencyValue(input));
