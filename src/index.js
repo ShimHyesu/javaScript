5d169f5e97aa46cda8fe350a580cd875e36fe8d0
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-19 15:00:23 +0900

    core_dataType_array 내림차순으로 정렬하기

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import "./styles.css";
 
-function filterRangeInPlace(arr, a, b) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < a || arr[i] > b) {
-      arr.splice(i, 1);
-      i--; //범위 밖의 요소가 연속으로 나왔을 시, 제거
-    }
-  }
-}
+let arr = [5, 2, 1, -10, 8];
 
-let arr = [5, 3, 8, 1, 10, 11, 12];
+// 요소를 내림차순으로 정렬해주는 코드를 여기에 작성해보세요.
+/*
+arr.sort((a, b) => a - b);
+arr.reverse();
+*/
+arr.sort((a, b) => b - a);
 
-filterRangeInPlace(arr, 1, 4); // 1과 4 사이에 있지 않은 요소는 모두 제거함
-
-alert(arr); // [3, 1]
+alert(arr); // 8, 5, 2, 1, -10
