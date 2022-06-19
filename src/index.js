23f768d33f0cfc6ac62851b77a80e67db9bdd58e
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-19 22:42:16 +0900

    core_dataType_array 배열 요소 무작위로 섞기

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 import "./styles.css";
 
-function sortByAge(users) {
-  /*
-  users.sort(function (a, b) {
-    if (a.age > b.age) return 1;
-    if (a.age < b.age) return -1;
-    return 0;
-  });
-  */
-  users.sort((a, b) => a.age - b.age);
+function shuffle(arr) {
+  let newArr = [];
+  let rand = [];
+  //인덱스를 랜덤으로
+  for (let i = 0; i < arr.length; i++) {
+    rand[i] = Math.round(Math.random() * 10) % arr.length;
+    for (let j = 0; j < i; j++) {
+      if (rand[i] === rand[j]) i--;
+    }
+    newArr[i] = arr[rand[i]];
+  }
+
+  return newArr;
 }
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+// 1, 2, 3으로 만들 수 있는 모든 순열의 빈도를 세줍니다.
+let count = {
+  "123": 0,
+  "132": 0,
+  "213": 0,
+  "231": 0,
+  "321": 0,
+  "312": 0
+};
 
-let arr = [pete, john, mary];
+for (let i = 0; i < 10000; i++) {
+  let array = [1, 2, 3];
+  let newArr = shuffle(array);
+  count[newArr.join("")]++;
+}
 
-sortByAge(arr);
-
-// now: [john, mary, pete]
-alert(arr[0].name); // John
-alert(arr[1].name); // Mary
-alert(arr[2].name); // Pete
+// 만들 수 있는 모든 순열의 생성 빈도를 세서 출력해줍니다.
+for (let key in count) {
+  alert(`${key}: ${count[key]}`);
+}
