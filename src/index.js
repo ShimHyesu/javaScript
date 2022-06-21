40eeab05d17d9550993e781a1ecef2ab4dc389d5
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-21 09:35:30 +0900

    lecture_prototype 객체 생성자

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import "./styles.css";
 
-function getAverageAge(arr) {
-  return arr.reduce((sum, current) => sum + current.age, 0) / arr.length;
+function Animal(type, name, sound) {
+  this.type = type;
+  this.name = name;
+  this.sound = sound;
 }
 
-let john = { name: "John", age: 25 };
-let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 29 };
+Animal.prototype.say = function () {
+  console.log(this.sound);
+};
 
-let arr = [john, pete, mary];
+//어떤 값을 재사용하고 싶을때
+//prototype의 역할: 객체 생성자로 무엇을 만들었을 때
+//그걸로 만든 객체끼리 공유할 수 있는 값이나 함수
+Animal.prototype.sharedValue = 1;
 
-alert(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
+const dog = new Animal("개", "멍멍이", "멍멍");
+const cat = new Animal("고양이", "야옹이", "야옹");
+
+// function say() {
+//   console.log(this.sound);
+// }
+// dog.say = say;
+// cat.say = say;
+
+dog.say();
+cat.say();
+dog.sharedValue;
