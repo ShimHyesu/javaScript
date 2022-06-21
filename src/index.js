d329c68ccb99073794408d3f2f55778f5dcecbed
ShimHyesu <58768930+ShimHyesu@users.noreply.github.com>
2022-06-21 10:08:16 +0900

    lecture_class ES6 Class

--- src/index.js
+++ src/index.js
@@ -1,47 +1,43 @@
 import "./styles.css";
 
-// function Dog(name, sound) {
-//   this.type = "개";
-//   this.name = name;
-//   this.sound = sound;
-// }
-// function Cat(name, sound) {
-//   this.type = "고양이";
-//   this.name = name;
-//   this.sound = sound;
-// }
-
-// Dog.prototype.say = function () {
-//   console.log(this.sound);
-// };
-
-function Animal(type, name, sound) {
-  this.type = type;
-  this.name = name;
-  this.sound = sound;
+//자바스크립트에서는 ES6부터 class문법 도입됨
+class Animal {
+  //생성자의 역할
+  constructor(type, name, sound) {
+    this.type = type;
+    this.name = name;
+    this.sound = sound;
+  }
+  //함수를 만들게되면 자동으로 프로토타입으로 등록됨
+  say() {
+    console.log(this.sound);
+  }
 }
 
-Animal.prototype.say = function () {
-  console.log(this.sound);
-};
+console.log(Animal.prototype.say);
 
-function Dog(name, sound) {
-  Animal.call(this, "개", name, sound);
-}
-//call(this,param)
-//this: 이 객체 생성자 함수에서의 this
-//param: 상속하는 생성자의 파타미터
-
-function Cat(name, sound) {
-  Animal.call(this, "고양이", name, sound);
+//상속 훨씬 더 쉽게할 수 있음
+class Dog extends Animal {
+  //constructor만 선언하면 됨
+  // ->기존 Animal에서 사용하는 constructor덮어씀
+  // -> super키워드: Animal가지고 있는 consturctor 먼저 호출 후 자신 처리
+  constructor(name, sound) {
+    super("개", name, sound);
+  }
 }
 
-//프로토타입 공유
-Dog.prototype = Animal.prototype;
-Cat.prototype = Animal.prototype;
+class Cat extends Animal {
+  constructor(name, sound) {
+    super("고양이", name, sound);
+  }
+}
 
 const dog = new Dog("멍멍이", "멍멍");
 const cat = new Cat("야옹이", "야옹");
+const cat2 = new Cat("야오옹이", "야오오옹");
 
 dog.say();
 cat.say();
+cat2.say();
+
+//결국 객체 생성자와 프로토타입을 조금 더 쉽게 사용하기 위해 만들어진 문법임
